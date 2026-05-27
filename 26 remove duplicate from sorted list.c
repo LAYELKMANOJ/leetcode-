d27333fbd483d0daa07e32f39f02dc22fd86df55
LAYEL K MANOJ <layelkmanoj@gmail.com>
2026-05-27 09:02:40 +0530

    Update 26 remove duplicate from sorted list.c

--- 26 remove duplicate from sorted list.c
+++ 26 remove duplicate from sorted list.c
@@ -4,13 +4,13 @@ struct ListNode* deleteDuplicates(struct ListNode* head) {
 
     while (current != NULL && current->next != NULL) {
         if (current->val == current->next->val) {
-            // Skip duplicate node
+   
             current->next = current->next->next;
         } else {
-            // Move to next node
+       
             current = current->next;
         }
     }
-
+    
     return head;
 }
